ff02fa2722fc872c231a883ce65cdbfdff130ac6
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-10-17 09:18:35 +0530

    Add files via upload

---
+++ Class_Work/Day_4/program62.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int iNo = 7236;
+    int iDigit = 0;
+
+    printf("The Original number is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    return 0;
+}
+
