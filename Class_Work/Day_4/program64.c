5047a343fd036010262106280a7a0d014f144e9a
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-10-17 09:20:29 +0530

    Add files via upload

---
+++ Class_Work/Day_4/program64.c
@@ -0,0 +1,50 @@
+// code to demonstrate the need of iteration
+
+#include<stdio.h>
+
+int main()
+{
+    int iNo = 723614;
+    int iDigit = 0;
+
+    printf("The Original number is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    printf("-----------------------------------------------------\n");
+    iDigit = iNo % 10;
+    printf("iDigit is : %d\n", iDigit);
+    iNo = iNo / 10;
+    printf("iNo is : %d\n", iNo);
+
+    return 0;
+}
+
