df84f4a1466338a7385293314fba98677f184b1e
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-10-17 09:18:00 +0530

    Add files via upload

---
+++ Class_Work/Day_4/program59.c
@@ -0,0 +1,49 @@
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int CheckPrime(int iNo)
+{
+    int iCnt = 0;    //Loop Counter
+    bool bFlag = false;
+
+    if(iNo<0)
+    {
+        iNo = -iNo;
+    }
+  
+    for(iCnt = 2, bFlag = true; iCnt<=(iNo/2); iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            bFlag = false;
+            break;                      
+        }
+    }
+
+    return bFlag;
+
+}
+
+int main()
+{
+    int ivalue = 0;
+    
+    int bRet = false;
+
+    printf("Enter the Number : \n");
+    scanf("%d",&ivalue);
+    
+    bRet = CheckPrime(ivalue);
+
+    if(bRet == true)
+    {
+        printf("%d is prime number", ivalue);
+    }
+    else
+    {
+        printf("%d is not prime number", ivalue);
+    }
+
+    return 0;
+}
