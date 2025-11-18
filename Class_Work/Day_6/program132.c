d3e85e84237b638ddd816a39b2bf68a9018b5831
Nikhil Ahire <159749928+Nikhil1620@users.noreply.github.com>
2025-11-18 11:19:35 +0530

    Add files via upload

---
+++ Class_Work/Day_6/program132.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+float Average(int Arr[], int iSize)
+{
+    int iCnt = 0;
+
+    int iSum = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)    
+    {
+        iSum = iSum + Arr[iCnt];
+    }
+
+    return ((float)iSum / (float)iSize);
+}
+
+int main()
+{
+    int iLength = 0;
+    int iCnt = 0;
+    int *ptr = NULL;
+    float fResult = 0;
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &iLength);
+
+    ptr = (int *)malloc(iLength * sizeof(int));
+    
+    if(ptr == NULL)      
+     {
+        printf("Unable to allocate the memoey\n");
+        return -1;
+    }
+
+    printf("Enter the elements : \n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+
+    fResult = Average(ptr,iLength);
+    printf("The Average of the given array are : %f", fResult);
+
+    free(ptr);
+
+    return 0;
+}
